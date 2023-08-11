c56e43f5054e564fd94d9640f9e3442924ec592b
khuong <khuonghumg1376@gmail.com>
2023-08-11 17:33:25 +0700

    add function setInterval

--- sleep.js
+++ sleep.js
@@ -10,5 +10,31 @@ async function counts(times) {
     await sleep(1000);
     console.log(i);
   }
+  console.log("hi");
 }
-console.log(counts(10));
+// console.log(counts(5));
+
+function countUp(num) {
+  return new Promise((resolve) => {
+    let times = setInterval(() => {
+      console.log(num);
+      ++num;
+      if (num > 10) {
+        clearInterval(times);
+        resolve();
+      }
+    }, 1000);
+  });
+}
+// console.log(countUp(1));
+
+function countDown(x) {
+  let loop = setInterval(() => {
+    console.log(x);
+    if (x < 1) {
+      clearInterval(loop);
+    }
+    --x;
+  }, 1000);
+}
+console.log(countDown(7));
